720846868695d06075e6f8567c2fc1852338707f
tianyn <ty468123@163.com>
2020-11-10 09:52:31 +0800

    update:rabbitmq

--- springboot-components/src/main/java/com/tionsy/springbootcomponents/listener/rabbitMq/MyRabbitMListener.java
+++ springboot-components/src/main/java/com/tionsy/springbootcomponents/listener/rabbitMq/MyRabbitMListener.java
@@ -1,13 +1,9 @@
 package com.tionsy.springbootcomponents.listener.rabbitMq;
 
-import com.alibaba.fastjson.JSON;
-import com.rabbitmq.client.AMQP;
 import com.tionsy.springbootcomponents.mq.ConsumerQueue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
